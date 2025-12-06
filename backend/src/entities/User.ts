4643c4ccc98725eaa703d36140521e09cf88f596
Ana Michelle Lopez Hernandez <ing.amichellelopezhdz@gmail.com>
2025-12-05 18:52:47 -0600

    create typeorm datasource, add user entity schema and add initial database migration for user entity

---
+++ backend/src/entities/User.ts
@@ -0,0 +1,52 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
+} from 'typeorm';
+import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
+import * as bcrypt from 'bcryptjs';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email!: string;
+
+  @Column()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  firstName!: string;
+
+  @Column()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  lastName!: string;
+
+  @Column({ select: false }) // select: false para no devolver el password en queries normales
+  password!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  // Hash del password antes de insert o update 
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (this.password) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+  }
+
+}
